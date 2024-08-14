0d43bff0da8c0690cafb3ad6ad94b1eb35a93dac
jaexxin <jmsb02@naver.com>
2024-08-14 15:06:14 +0900

    ParkingSpacesService

---
+++ src/main/java/JARAMIOT/jaram/parkingspaces/entity/ParkingSpacesStatus.java
@@ -0,0 +1,7 @@
+package JARAMIOT.jaram.parkingspaces.entity;
+
+public enum ParkingSpacesStatus {
+    AVAILABLE, //사용 가능
+    RESERVED, //사용 중
+    UNAVAILABLE //사용 불가능
+}
--- src/main/java/JARAMIOT/jaram/parkingspaces/service/ParkingSpacesService.java
+++ src/main/java/JARAMIOT/jaram/parkingspaces/service/ParkingSpacesService.java
@@ -1,4 +1,34 @@
 package JARAMIOT.jaram.parkingspaces.service;
 
+import JARAMIOT.jaram.parkingspaces.entity.ParkingSpaces;
+import JARAMIOT.jaram.parkingspaces.entity.ParkingSpacesStatus;
+
+import java.util.List;
+
 public interface ParkingSpacesService {
+
+    //주차 공간 생성
+    ParkingSpaces createParkingSpace(ParkingSpaces parkingSpaces);
+
+    //전체 주차 공간 조회
+    List<ParkingSpaces> getAllParkingSpaces();
+
+    //특정 주차 공간 조회
+    ParkingSpaces getParkingSpaceById(Long id);
+
+
+    //특정 위치의 주차 공간 조회
+    List<ParkingSpaces> getParkingSpacesByLocation(String location);
+
+    //주차 공간 업데이트
+    ParkingSpaces updateParkingSpace(Long id, ParkingSpaces updatedParkingSpace);
+
+
+    //주차 공간 상태 변경
+    ParkingSpaces changeParkingSpaceStatus(Long id, ParkingSpacesStatus status);
+
+    //주차 공간 삭제
+    void deleteParkingSpace(Long id);
+
+
 }
