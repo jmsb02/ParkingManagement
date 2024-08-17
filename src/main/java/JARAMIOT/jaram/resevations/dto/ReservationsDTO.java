a5d83be528f4b07883679b70b0e7c7869374bca7
jaexxin <jmsb02@naver.com>
2024-08-17 22:24:57 +0900

    ReservationDTO 수정

--- src/main/java/JARAMIOT/jaram/resevations/dto/ReservationsDTO.java
+++ src/main/java/JARAMIOT/jaram/resevations/dto/ReservationsDTO.java
@@ -1,14 +1,13 @@
 package JARAMIOT.jaram.resevations.dto;
 
-import JARAMIOT.jaram.resevations.entity.Reservations;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
-@AllArgsConstructor
 @NoArgsConstructor
+@AllArgsConstructor
 public class ReservationsDTO {
 
 
@@ -18,8 +17,4 @@ public class ReservationsDTO {
     @NotNull
     private Long parkingSpaceId;
 
-    public ReservationsDTO(Long userId, Long parkingSpaceId) {
-        this.userId = userId;
-        this.parkingSpaceId = parkingSpaceId;
-    }
 }
